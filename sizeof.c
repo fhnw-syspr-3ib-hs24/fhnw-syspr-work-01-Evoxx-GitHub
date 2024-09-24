8ff7494e3bc953c85edf1012073d4d4e9ace7ec3
Evoxx-GitHub <s.christen@gmx.ch>
2024-09-24 16:50:03 +0200

    solved a

---
+++ sizeof.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Ausgabe der Größe der Basistypen
+    printf("sizeof(char)   = %d Byte\n", (int) sizeof(char));
+    printf("sizeof(int)    = %d Byte\n", (int) sizeof(int));
+    printf("sizeof(long)   = %d Byte\n", (int) sizeof(long));
+    printf("sizeof(float)  = %d Byte\n", (int) sizeof(float));
+    printf("sizeof(double) = %d Byte\n", (int) sizeof(double));
+
+    return 0;
+}
